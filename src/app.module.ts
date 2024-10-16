8eac72785734a65f3aa201f3432d8ade3986a1da
Nijat Ismayilov <nijat.ismayilov@outlook.com>
2024-10-17 00:37:11 +0100

    DB updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ dotenv.config();
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: 3306,
+      port: 22943,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
